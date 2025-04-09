15661d54ec49d9419063c849e690b619ea54853d
AntonioJesusRM <antjrm95@gmail.com>
2025-04-09 15:26:30 +0200

    Init SonarQube:  -Add usesCleartextTraffic="false" to prevent insecure connections (http)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.sonarPluggin)
 }
 
 android {
